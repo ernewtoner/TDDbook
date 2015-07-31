d4042150667cecd2b3dd75ebefb206a50c170607
Eric <ilswyn@gmail.com>
2015-07-30 20:11:13 -0700

    Functional test now checks we can input a to-do item 2

--- lists/tests.py
+++ lists/tests.py
@@ -5,10 +5,6 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 # Create your tests here.
-#class SmokeTest(TestCase):
-
-    #def test_bad_maths(self):
-     #   self.assertEqual(1 + 1, 3)
 class HomePageTest(TestCase):
 
     def test_root_url_resloves_to_home_page_view(self):
@@ -19,6 +15,5 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
